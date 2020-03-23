ff2f67d06ec077252645514e053a4a311a8d3289
Teresa Carbajal <Tere@MacBook-Air-de-Teresa.local>
2020-03-23 12:09:20 -0600

    home details

--- src/view/home.js
+++ src/view/home.js
@@ -6,7 +6,7 @@ export default (user) => {
   const viewPost = document.createElement('div');
   viewPost.innerHTML = `
   <form id="form-addPost">
-      <button class="buttons" type="submit" id="btnClosed"><a href="#/welcome">Sign Out</a></button></br>
+      <button class="buttons-h" type="submit" id="btnClosed"><a href="#/welcome">Sign Out</a></button></br>
     
       <h2 class="text-center">${user.email}</h2>
          <figure class="text-center">
@@ -14,11 +14,12 @@ export default (user) => {
           </figure>
 
             <input name="post" type="text" id="addPost" placeholder="¿Qué te gustaría compartir hoy?"></br>
-            <button type="submit" id="btn-addPost"><a href="#/home">Publicar</button></br>
-      
-             <div>
-              <button> Galeria </button>
-                <select>
+           
+            <button id="btn-addPost"><a href="#/home">Publicar</button></br>
+            
+            <div class="div-home">
+              <button class="buttons-h"> Galeria </button>
+                <select class="buttons-h">
                    <option>Publico</option>
                   <option>Privado</option>
                </select>
